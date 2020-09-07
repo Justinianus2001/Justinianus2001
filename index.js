42331903ce18f8570807bf0cbdbb122f383dec97
Justinianus <lengochoang681@gmail.com>
2020-09-07 15:09:36 +0700

    Generate Code

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ const generate = async () => {
   const { quote, author } = await getQuote();
 
   if (!quote) return;
-  
+
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, `0`);
   var mm = String(today.getMonth() + 1).padStart(2, `0`);
   var yyyy = today.getFullYear();
-  
+
   today = mm + `/` + dd + `/` + yyyy;
   fs.writeFileSync("README.md", `*Quote of the Day (${today}):*\n\n_**${quote}**_\n\n${author}`);
 };
