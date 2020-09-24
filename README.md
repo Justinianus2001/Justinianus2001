41adc8d8e5c54b19b2c8e58544c0e5d527b400c3
Justinianus2001 <lengochoang681@gmail.com>
2020-09-24 09:28:40 +0700

    Generate Code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 _**A loser doesn't know what he'll do if he loses, but talks about what he'll do if he wins, and a winner doesn't talk about what he'll do if he wins, but knows what he'll do if he loses.**_
 
-null
+Anonymous
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const getQuote = async () => {
   try {
     const { data } = await axios.get("https://quotes.rest/qod?language=en");
     const quote = data.contents.quotes[0].quote;
-    const author = data.contents.quotes[0].author;
+    const author = data.contents.quotes[0].author === null ? `Anonymous` : data.contents.quotes[0].author;
 
     console.log("new quote", `"${quote}"`);
+	console.log("author", `"${author}"`);
 
     return {
       quote,
@@ -23,16 +24,14 @@ const generate = async () => {
   const { quote, author } = await getQuote();
 
   if (!quote)
-	  return;
-  if (!author)
-	  author = `Anonymous`
+	return;
 
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, `0`);
-  var mm = String(today.getMonth() + 1).padStart(2, `0`);
-  var yyyy = today.getFullYear();
+  const curDate = new Date();
+  const dd = String(curDate.getDate()).padStart(2, `0`);
+  const mm = String(curDate.getMonth() + 1).padStart(2, `0`);
+  const yyyy = String(curDate.getFullYear()).padStart(4, `0`);
 
-  today = mm + `/` + dd + `/` + yyyy;
+  const today = mm + `/` + dd + `/` + yyyy;
   fs.writeFileSync("README.md", `*Quote of the Day (${today}):*\n\n_**${quote}**_\n\n${author}`);
 };
 
